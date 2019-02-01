21013270ab0ef3e19e60928b519f5866cc1682f5
rrkkee015 <pok_winter@naver.com>
2019-02-01 16:43:47 +0900

    2019-02-01

--- SSAFY/week8/Day4/홍길동/ex.py
+++ SSAFY/week8/Day4/홍길동/ex.py
@@ -1,8 +1,7 @@
 # 파일명 변경 금지
 # 아래에 클래스 Point와 Circle을 선언하세요.
-
 class Point:
-    def __init__(self, x, y):
+    def __init__(self,x,y):
         self.x=x
         self.y=y
 
@@ -12,16 +11,7 @@ class Circle:
         self.r=r
     
     def get_area(self):
-        return round(3.14*self.r*self.r,2)
-    
-    def get_perimeter(self):
-        return round(3.14*2*self.r,2)
-    
-    def get_center(self):
-        return (self.center.x,self.center.y)
-    
-    def print(self):
-        print(f'Circle: ({self.center.x}, {self.center.y}), r: {self.r}')
+        return 3.14
 
 # 아래의 코드는 수정하지마세요. 
 p1 = Point(0, 0)
