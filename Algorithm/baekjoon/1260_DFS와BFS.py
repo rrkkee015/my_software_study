18ae5959ef09505a4c4ac035e46a5b39011b292b
rrkkee015 <pok_winter@naver.com>
2019-02-27 01:57:32 +0900

    2019-02-27

---
+++ Algorithm/baekjoon/1260_DFS와BFS.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self,item):
+        self.item=item
+        self.next=None
+
+def eQ(item):
+    global front, end
+    newnode=Node(item)
+    if front==None:
+        front=newnode
+    else:
+        end.next=newnode
+    end=newnode
+
+def dQ():
+    global front, end
+    if front==None:
+        return 'empty'
+    item=front.item
+    front=front.next
+    if front==None:
+        end=None
+    return item
+
+def search(i):
+    print(i,end=' ')
+    for j in range(N+1):
+        if mat[i][j] == 1 and visited[j]==False:
+            visited[j]=True
+            search(j)
+
+
+N,M,V=list(map(int,input().split()))
+mat=[[0]*(N+1) for i in range(N+1)]
+visited=[False]*(N+1)
+front=None
+end=None
+for i in range(M):
+    s,g=list(map(int,input().split()))
+    mat[s][g]=1
+    mat[g][s]=1
+visited[V]=True
+search(V)
+print()
+visited=[False]*(N+1)
+eQ(V)
+visited[V]=True
+while True:
+    take=dQ()
+    if take=='empty':
+        break
+    print(take, end=' ')
+    for i in range(N+1):
+        if mat[take][i] == 1 and visited[i]==False:
+            eQ(i)
+            visited[i]=True
--- Algorithm/baekjoon/2178_미로_탐색.py
+++ Algorithm/baekjoon/2178_미로_탐색.py
@@ -1,17 +1,46 @@
-def search(y,x,cnt):
-    if y==N-1 and x==M-1:
-        print(cnt)
-    else:
-        for k in dxdy:
-            if 0<=y+k[0]<=N-1 and 0<=x+k[1]<=M-1 and Ma[y+k[0]][x+k[1]]==1:
-                Ma[y+k[0]][x+k[1]]=0
-                cnt+=1
-                search(y+k[0],x+k[1],cnt)
+class Node:
+    def __init__(self,item):
+        self.item=item
+        self.next=None
 
-N,M=tuple(map(int,input().split()))
-Ma=[[int(i) for i in input()] for j in range(N)]
-cnt=0
+def eq(item):
+    global front, end
+    newnode=Node(item)
+    if front==None:
+        front=newnode
+    else:
+        end.next=newnode
+    end=newnode
+
+def dq():
+    global front, end
+    if front==None:
+        return 'empty'
+    item=front.item
+    front=front.next
+    if front==None:
+        end=None
+    return item
+
+front,end=(None,None)
+N,M=list(map(int,input().split()))
+maze=[[int(i) for i in input()] for k in range(N)]
+eq([0,0,1])
+maze[0][0]=0
 dxdy=[(1,0),(0,-1),(-1,0),(0,1)]
-Ma[0][0]=0
-cnt+=1
-search(0,0,cnt)
+out=True
+while out:
+    take=dq()
+    y=take[0]
+    x=take[1]
+    cnt=take[2]
+    if (y,x)==(N-1,M-1):
+        result=cnt
+        break
+    for i in dxdy:
+        if 0<=y+i[0]<N and 0<=x+i[1]<M and maze[y+i[0]][x+i[1]]==1:
+            maze[y+i[0]][x+i[1]]=0
+            a=y+i[0]
+            b=x+i[1]
+            eq([a,b,cnt+1])
+print(result)
