499f0f05f99e739af694cd5377877aa9ac8de456
rrkkee015 <pok_winter@naver.com>
2019-03-19 17:01:25 +0900

    2019-03-19

--- Algorithm/baekjoon/16922_로마_숫자_만들기.py
+++ Algorithm/baekjoon/16922_로마_숫자_만들기.py
@@ -1,34 +1,36 @@
+# N=int(input())
+# cnt=0
+# roma=[0,0,0,0]
+# nums=[1,5,10,50]
+# visited=[]
+# for a in range(N+1):
+#     roma[0]=a
+#     if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
+#         cnt+=1
+#         roma[0]=0
+#         visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
+#         continue
+#     for b in range(N+1):
+#         roma[1]=b
+#         if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
+#             cnt+=1
+#             roma[1]=0
+#             visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
+#             continue
+#         for c in range(N+1):
+#             roma[2]=c
+#             if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
+#                 cnt+=1
+#                 roma[2]=0
+#                 visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
+#                 continue
+#             for d in range(N+1):
+#                 roma[3]=d
+#                 if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
+#                     cnt+=1
+#                     roma[3]=0
+#                     visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
+#                     continue
+# print(cnt)
+
 N=int(input())
-cnt=0
-roma=[0,0,0,0]
-nums=[1,5,10,50]
-visited=[]
-for a in range(N+1):
-    roma[0]=a
-    if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
-        cnt+=1
-        roma[0]=0
-        visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
-        continue
-    for b in range(N+1):
-        roma[1]=b
-        if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
-            cnt+=1
-            roma[1]=0
-            visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
-            continue
-        for c in range(N+1):
-            roma[2]=c
-            if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
-                cnt+=1
-                roma[2]=0
-                visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
-                continue
-            for d in range(N+1):
-                roma[3]=d
-                if sum(roma)==N and nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a not in visited:
-                    cnt+=1
-                    roma[3]=0
-                    visited.append(nums[3]*d+nums[2]*c+nums[1]*b+nums[0]*a)
-                    continue
-print(cnt)
