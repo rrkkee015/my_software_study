cbe2f0e74a00c1a932a3d55193b7738816a30e99
rrkkee015 <pok_winter@naver.com>
2019-03-23 10:15:18 +0900

    2019-03-23

---
+++ Algorithm/baekjoon/11724_연결_요소의_개수.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10**9)
+def find(i):
+    visited[i]=False
+    for _ in range(len(mat[i])):
+        if visited[mat[i][_]]:
+            find(mat[i][_])
+
+N,M = list(map(int,input().split()))
+mat=[[] for _ in range(N+1)]
+for _ in range(M):
+    S,G = list(map(int,input().split()))
+    mat[S].append(G)
+    mat[G].append(S)
+visited=[True]*(N+1)
+cnt=0
+
+for x in range(1,len(mat)):
+    if visited[x]:
+        cnt+=1
+        find(x)
+print(cnt)
