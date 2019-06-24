c95d6f67b42e08ca851832bbf1d8b6bbe96cc99d
rrkkee015 <pok_winter@naver.com>
2019-06-24 17:33:08 +0900

    2019-06-24

--- Algorithm/baekjoon/1260_DFS와BFS.py
+++ Algorithm/baekjoon/1260_DFS와BFS.py
@@ -55,48 +55,115 @@
 #             eQ(i)
 #             visited[i]=True
 
-N, M, V = map(int,input().split())
+# N, M, V = map(int,input().split())
+# mat = [[] for _ in range(N + 1)]
+#
+# for _ in range(M):
+#     s, g = map(int,input().split())
+#     mat[s].append(g)
+#     mat[g].append(s)
+#
+# for _ in range(len(mat)):
+#     if mat[_]:
+#         mat[_] = sorted(mat[_])
+#
+# visited = [1 for _ in range(N + 1)]
+# dfs_result = []
+# def dfs(go):
+#     dfs_result.append(str(go))
+#     for _ in mat[go]:
+#         if visited[_]:
+#             visited[_] = 0
+#             dfs(_)
+# visited[V] = 0
+# dfs(V)
+# print(' '.join(dfs_result))
+#
+# import collections
+# dq = collections.deque()
+# visited = [1 for _ in range(N + 1)]
+# visited[V] = 0
+# dq.append(V)
+# bfs_result = []
+# while dq:
+#     v = dq.popleft()
+#     bfs_result.append(str(v))
+#     for _ in mat[v]:
+#         if visited[_]:
+#             visited[_] = 0
+#             dq.append(_)
+# print(' '.join(bfs_result))
+
+# from collections import deque
+#
+# N, M, V = list(map(int,input().split()))
+#
+# mat = [[] for _ in range(N+1)]
+# visited = [0 for _ in range(N+1)]
+# visited[V] = 1
+#
+# for _ in range(M):
+#     S, G = list(map(int,input().split()))
+#     mat[S].append(G)
+#     mat[G].append(S)
+#
+# DFS_result = [str(V)]
+#
+# def DFS(go):
+#     for _ in range(len(mat[go])):
+#         if visited[mat[go][_]] == 0:
+#             visited[mat[go][_]] = 1
+#             DFS_result.append(str(mat[go][_]))
+#             DFS(mat[go][_])
+# DFS(V)
+#
+# print(DFS_result)
+
+N, M, V = list(map(int, input().split()))
+
 mat = [[] for _ in range(N + 1)]
 
 for _ in range(M):
-    s, g = map(int,input().split())
-    mat[s].append(g)
-    mat[g].append(s)
+    S, G = list(map(int,input().split()))
+    mat[S].append(G)
+    mat[G].append(S)
 
 for _ in range(len(mat)):
-    if mat[_]:
-        mat[_] = sorted(mat[_])
+    mat[_] = sorted(mat[_])
 
 visited = [1 for _ in range(N + 1)]
-dfs_result = []
+visited[V] = 0
+dfs_result = [str(V)]
+
 def dfs(go):
-    dfs_result.append(str(go))
-    for _ in mat[go]:
-        if visited[_]:
-            visited[_] = 0
-            dfs(_)
-visited[V] = 0
+    for _ in range(len(mat[go])):
+        if visited[mat[go][_]]:
+            visited[mat[go][_]] = 0
+            dfs_result.append(str(mat[go][_]))
+            dfs(mat[go][_])
+
 dfs(V)
-print(' '.join(dfs_result))
 
-import collections
-dq = collections.deque()
-visited = [1 for _ in range(N + 1)]
+from collections import deque
+
+dq = deque()
+
+visited = [1 for _ in range(N+1)]
 visited[V] = 0
+
 dq.append(V)
-bfs_result = []
+bfs_result = [str(V)]
+
 while dq:
-    v = dq.popleft()
-    bfs_result.append(str(v))
-    for _ in mat[v]:
-        if visited[_]:
-            visited[_] = 0
-            dq.append(_)
+    go = dq.popleft()
+    for _ in range(len(mat[go])):
+        if visited[mat[go][_]]:
+            visited[mat[go][_]] = 0
+            dq.append(mat[go][_])
+            bfs_result.append(str(mat[go][_]))
+
+print(' '.join(dfs_result))
 print(' '.join(bfs_result))
-
-
-
-
 
 
 
